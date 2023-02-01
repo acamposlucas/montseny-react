b8dd2f77a4a012e233c2c1339392cbe43a7aa07b
lucasalmeida <lac.lucas96@gmail.com>
2023-02-01 16:42:43 -0300

    cria componente footer

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 export default function Root() {
@@ -8,9 +9,7 @@ export default function Root() {
     <main>
       <Outlet />
     </main>
-    <footer>
-      Footer
-    </footer>
+    <Footer />
     </>
   )
 }
